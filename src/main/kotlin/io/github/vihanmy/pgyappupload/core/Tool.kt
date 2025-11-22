afab533c464535b99f8f9fe3bc297ba416de79f1
vihanmy_dell <vihanmy@gmail.com>
2025-11-22 10:41:35 +0800

    ✨ (project): optimize file filtering rules and support apk file

--- src/main/kotlin/io/github/vihanmy/pgyappupload/core/Tool.kt
+++ src/main/kotlin/io/github/vihanmy/pgyappupload/core/Tool.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.isFile
 import io.github.vihanmy.pgyappupload.model.consts.PluginConfig
 import java.nio.file.Files
 import java.nio.file.attribute.BasicFileAttributes
@@ -23,10 +24,11 @@ object Tool {
     }
 
     fun chooseFileAndUpload(project: Project): VirtualFile? {
-        val descriptor = FileChooserDescriptor(true, false, false, false, false, false)
+        val descriptor = FileChooserDescriptor(true, true, true, true, true, false)
             .withTitle("选择要上传的安装包")
             .withDescription("支持安卓(.apk)和鸿蒙(.hap)")
-            .withFileFilter { it.extension in PluginConfig.supportAppFileExt }
+            .withShowHiddenFiles(true)
+            .withFileFilter { (it.extension in PluginConfig.supportAppFileExt) and (it.isFile) }
         //
         val chosenFiles = FileChooser.chooseFiles(descriptor, project, null)
         return chosenFiles.getOrNull(0)
